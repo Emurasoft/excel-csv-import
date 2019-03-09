e4d87c7bd48191a39d3e1f43b98d52eb63422b72
Makoto Emura <makoto@emurasoft.com>
2019-03-08 17:19:21 -0800

    .

--- src/ExcelAPI.ts
+++ src/ExcelAPI.ts
@@ -4,7 +4,33 @@ export class ExcelAPI {
         return new ExcelAPI();
     }
 
-    private constructor() {
+    public run(batch: (worksheet: Excel.Worksheet) => Promise<void>) {
+        Excel.run((context) => {
+            return batch(context.workbook.worksheets.getActiveWorksheet())
+                .then(() => context.sync());
+        });
+    }
 
+    public setChunk(worksheet: Excel.Worksheet, row: number, chunk: string[][]) {
+        // New range values must have the same shape as range
+        const maxLength = ExcelAPI.maxLength(chunk);
+        ExcelAPI.resize(chunk, maxLength);
+        worksheet.getRangeByIndexes(row, 0, chunk.length, maxLength).values = chunk;
+    }
+
+    private static resize(a: string[][], maxLength: number) {
+        for (const row of a) {
+            row.concat(new Array(maxLength - row.length));
+        }
+    }
+
+    private static maxLength(a: string[][]) {
+        let max = 0;
+        for (const row of a) {
+            if (row.length > max) {
+                max = row.length;
+            }
+        }
+        return max;
     }
 }
--- src/Parser.ts
+++ src/Parser.ts
@@ -1,8 +1,23 @@
 import {ExcelAPI} from './ExcelAPI';
+import * as Papa from 'papaparse';
 
 export class Parser {
     public static async init() {
         return new Parser(await ExcelAPI.init());
+    }
+
+    public import(file: File, config: Papa.ParseConfig) {
+        this._api.run((worksheet) => {
+            return new Promise((resolve) => {
+                let row = 0;
+                config.chunk = (chunk: Papa.ParseResult) => {
+                    row += chunk.data.length;
+                    this._api.setChunk(worksheet, row, chunk.data);
+                }
+                config.complete = resolve as any;
+                Papa.parse(file, config);
+            });
+        });
     }
 
     private _api: ExcelAPI;
@@ -19,5 +34,7 @@ Otherwise:
 Parse file
 Chunk callback:
     Send chunk to api with i
+    Save any errors
 Sync
+Return errors
  */
