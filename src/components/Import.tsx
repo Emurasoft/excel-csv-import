e94153c4318c80473fe3175fc7cd69d32b6c7e74
Makoto Emura <makoto@emurasoft.com>
2019-03-09 23:02:34 -0800

    .

--- src/components/Import.tsx
+++ src/components/Import.tsx
@@ -1,12 +1,16 @@
 import {Store} from '../Store';
 import * as React from 'react';
 import {connect} from '../connect';
-import {Dropdown, IDropdownOption, Label} from 'office-ui-fabric-react';
+import {Dropdown, IDropdownOption, Label, TextField} from 'office-ui-fabric-react';
 
 enum FileSource {file, textfield, url}
 
+enum NewlineSequence {CRLF, CR, LF}
+
 interface State {
     fileSource: FileSource;
+    delimiter: string;
+    newlineSequence: NewlineSequence;
 }
 
 class ImportComponent extends React.Component<{store: Store}, State> {
@@ -14,6 +18,8 @@ class ImportComponent extends React.Component<{store: Store}, State> {
         super(props);
         this.state = {
             fileSource: FileSource.file,
+            delimiter: "\u002c",
+            newlineSequence: NewlineSequence.CRLF,
         }
     }
 
@@ -33,19 +39,65 @@ class ImportComponent extends React.Component<{store: Store}, State> {
             },
         ];
 
+        const newlineSequeneceMenu: IDropdownOption[] = [
+            {
+                key: NewlineSequence.CRLF,
+                text: "\\r\\n",
+            },
+            {
+                key: NewlineSequence.CR,
+                text: "\\r",
+            },
+            {
+                key: NewlineSequence.LF,
+                text: "\\n",
+            },
+        ];
+
         return (
             <div>
-                <Label>
-                    Import type
-                    <Dropdown
-                        selectedKey={this.state.fileSource}
-                        options={fileSourceMenu}
-                        onChange={(_, o) => this.setState({fileSource: o.key as FileSource})}
-                    />
-                </Label>
-                {this.state.fileSource}
+                <Dropdown
+                    label="Import type"
+                    selectedKey={this.state.fileSource}
+                    options={fileSourceMenu}
+                    onChange={(_, option) => this.setState({fileSource: option.key as FileSource})}
+                />
+                <br />
+                <TextField
+                    label="Delimiter"
+                    value={this.state.delimiter}
+                    description={ImportComponent.delimiterDescription(this.state.delimiter)}
+                    onChange={(_, value) => this.setState({delimiter: value})}
+                    onGetErrorMessage={ImportComponent.validateDelimiter}
+                    deferredValidationTime={20}
+                />
+                <br />
+                <Dropdown
+                    label="Newline sequence"
+                    selectedKey={this.state.newlineSequence}
+                    options={newlineSequeneceMenu}
+                    onChange={(_, option) => {
+                        this.setState({newlineSequence: option.key as NewlineSequence})
+                    }}
+                />
             </div>
-        );
+        );// TODO monospace where needed
+    }
+
+    private static delimiterDescription(delimiter: string) {
+        if (delimiter.length == 0) {
+            return "Auto-detect";
+        } else {
+            return delimiter;
+        }
+    }
+
+    private static validateDelimiter(value: string) {
+        if (value.length > 1) {
+            return "Delimiter length must be 0 or 1";
+        } else {
+            return "";
+        }
     }
 }
 
