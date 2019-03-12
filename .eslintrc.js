d29796b79019342014a44959fcd8718f8eb02116
Makoto Emura <makoto@emurasoft.com>
2019-03-12 11:51:49 -0700

    .

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,15 @@
 module.exports = {
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint'],
-    extends: ['plugin:@typescript-eslint/recommended'],
-    "overrides": [
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    'overrides': [
         {
-            "files": ["*.js"],
-            "rules": {
-                "@typescript-eslint/no-var-requires": 0,
+            'files': ['*.js'],
+            'env': {
+                'node': true,
+            },
+            'rules': {
+                '@typescript-eslint/no-var-requires': 0,
             },
         },
     ],
