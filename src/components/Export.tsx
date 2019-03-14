ce66f8338aafe0c05842c6e556cccdc12812f242
Makoto Emura <makoto@emurasoft.com>
2019-03-13 22:28:30 -0700

    .

--- src/components/Export.tsx
+++ src/components/Export.tsx
@@ -2,9 +2,11 @@ import {Store} from '../Store';
 import * as React from 'react';
 import {connect} from '../connect';
 import {ExportType, ExportTypeDropdown} from './ExportTypeDropdown';
+import {DelimiterDropdown} from './DelimiterDropdown';
 
 interface State {
     exportType: ExportType;
+    delimiter: string;
 }
 
 class ExportComponent extends React.Component<{store: Store}, State> {
@@ -12,15 +14,22 @@ class ExportComponent extends React.Component<{store: Store}, State> {
         super(props);
         this.state = {
             exportType: ExportType.file,
+            delimiter: ',',
         };
     }
 
     public render() {
         return (
-            <ExportTypeDropdown
-                value={this.state.exportType}
-                onChange={(exportType) => this.setState({exportType})}
-            />
+            <>
+                <ExportTypeDropdown
+                    value={this.state.exportType}
+                    onChange={(exportType) => this.setState({exportType})}
+                />
+                <DelimiterDropdown
+                    value={this.state.delimiter}
+                    onChange={(delimiter) => this.setState({delimiter})}
+                />
+            </>
         );
     }
 }
