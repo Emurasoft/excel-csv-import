383a5081bbaaac267279a0a59bb8af2dcfa3fde3
Makoto Emura <makoto@emurasoft.com>
2019-03-13 22:18:29 -0700

    .

--- src/components/export/Export.tsx
+++ src/components/export/Export.tsx
@@ -1,11 +1,26 @@
 import {Store} from '../../Store';
 import * as React from 'react';
 import {connect} from '../../connect';
+import {ExportType, ExportTypeDropdown} from './ExportTypeDropdown';
 
-class ExportComponent extends React.Component<{store: Store}, {}> {
+interface State {
+    exportType: ExportType;
+}
+
+class ExportComponent extends React.Component<{store: Store}, State> {
+    public constructor(props: {store: Store}) {
+        super(props);
+        this.state = {
+            exportType: ExportType.file,
+        };
+    }
+
     public render() {
         return (
-            <div>export page</div>
+            <ExportTypeDropdown
+                value={this.state.exportType}
+                onChange={(exportType) => this.setState({exportType})}
+            />
         );
     }
 }
