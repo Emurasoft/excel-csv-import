f5b0bff501b700e0ed81421a4d051f28ad37147f
Makoto Emura <makoto@emurasoft.com>
2019-03-15 17:41:42 -0700

    .

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,16 @@ import {initializeIcons} from 'office-ui-fabric-react/lib/Icons';
 import {Import} from './components/Import';
 import {BrowserRouter, Route} from 'react-router-dom';
 import {Export} from './components/Export';
+import {Store} from './Store';
 
 initializeIcons();
 
 ReactDOM.render(
     <BrowserRouter>
-        <>
+        <Store>
             <Route path='/import' component={Import} />
             <Route path='/export' component={Export} />
-        </>
+        </Store>
     </BrowserRouter>,
     document.getElementById('root'),
 );
