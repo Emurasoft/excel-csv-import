92e88fcd5fccc6be91510808d36fcb2bc9ec15db
Makoto Emura <makoto@emurasoft.com>
2019-03-15 17:36:21 -0700

    .

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {initializeIcons} from 'office-ui-fabric-react/lib/Icons';
 import {Import} from './components/Import';
-import {BrowserRouter, Link, Route} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import {Export} from './components/Export';
 
 initializeIcons();
