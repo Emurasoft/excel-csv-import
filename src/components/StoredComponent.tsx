0cb92a5dea5b44f554ea7b20922b4a8cbd793a11
Makoto Emura <makoto@emurasoft.com>
2019-03-21 19:18:08 -0700

    .

--- src/components/StoredComponent.tsx
+++ src/components/StoredComponent.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 
 export class StoredComponent<P = {}, S = {}> extends React.Component<P, S> {
-    public constructor(props: P, namespace: string, saveKeys: string[]) {
+    public constructor(props: P, namespace: string, savedKeys: string[]) {
         super(props);
         this._namespace = namespace;
-        this._saveKeys = saveKeys;
+        this._savedKeys = savedKeys;
 
         if (!localStorage) {
             // @ts-ignore
@@ -21,7 +21,7 @@ export class StoredComponent<P = {}, S = {}> extends React.Component<P, S> {
         for (const entry of Object.entries(localStorage)) {
             if (
                 entry[0].substring(0, this._namespace.length + 1) === this._namespace + '-'
-                && this._saveKeys.includes(entry[0].substring(this._namespace.length + 1))
+                && this._savedKeys.includes(entry[0].substring(this._namespace.length + 1))
             ) {
                 loadedState[entry[0].substring(this._namespace.length + 1)] = JSON.parse(entry[1]);
             }
@@ -37,7 +37,7 @@ export class StoredComponent<P = {}, S = {}> extends React.Component<P, S> {
         super.setState(state);
         if (typeof state === 'object') {
             for (const entry of Object.entries(state)) {
-                if (this._saveKeys.includes(entry[0])) {
+                if (this._savedKeys.includes(entry[0])) {
                     const key = this._namespace + '-' + entry[0];
                     localStorage.setItem(key, JSON.stringify(entry[1]));
                 }
@@ -46,5 +46,5 @@ export class StoredComponent<P = {}, S = {}> extends React.Component<P, S> {
     }
 
     private readonly _namespace: string;
-    private readonly _saveKeys: string[];
+    private readonly _savedKeys: string[];
 }
