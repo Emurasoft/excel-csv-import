9a2c6a656471646214baf203f44e905d6daca719
Makoto Emura <makoto@emurasoft.com>
2019-03-21 23:48:50 -0700

    .

--- src/AbortFlag.ts
+++ src/AbortFlag.ts
@@ -15,31 +15,22 @@ export class AbortFlag {
 }
 
 export class AbortFlagArray {
-    // Is this over-engineered? Yes, probably.
     public constructor() {
         this._array = [];
-        this._closed = false;
     }
 
     public newFlag(): AbortFlag {
         const flag = new AbortFlag();
-        if (this._closed) {
-            flag.abort();
-        } else {
-            this._array.push(flag);
-        }
+        this._array.push(flag);
         return flag;
     }
 
     public abort(): void {
-        this._closed = true;
         for (const item of this._array) {
             item.abort();
         }
         this._array = [];
-        this._closed = false;
     }
 
     private _array: AbortFlag[];
-    private _closed: boolean;
 }
