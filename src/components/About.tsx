0af006f02e02c3a205f92f4b026770a2d1b56509
Makoto Emura <makoto@emurasoft.com>
2019-03-22 22:43:07 -0700

    .

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,6 +6,7 @@ import * as FileSaver from 'file-saver';
 import {MemoryHistory} from 'history';
 import * as style from './style.css';
 
+/* eslint-disable max-len */
 export class AboutComponent extends React.Component<{store: Store; history: MemoryHistory}, {}> {
     public render(): React.ReactNode {
         return (
@@ -32,7 +33,7 @@ export class AboutComponent extends React.Component<{store: Store; history: Memo
                 <br /><br />
                 <div className={style.fullWidth + ' ' + style.centerContent}>
                     <a href='https://www.emeditor.com/'>
-                        <img className={style.emeditorLogo} src='emeditor_logo.png' alt='EmEditor logo' />
+                        <img className={style.emeditorLogo} src={'emeditor_logo.png'} alt='EmEditor logo' />
                     </a>
                 </div>
                 <Text variant='medium'>
@@ -70,9 +71,9 @@ export class AboutComponent extends React.Component<{store: Store; history: Memo
                     &#10;
                     &#10;Copyright 2019 Emurasoft Inc.
                     &#10;
-                    &#10;Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+                    &#10;Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the &quot;Software&quot;), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
                     &#10;The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-                    &#10;THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+                    &#10;THE SOFTWARE IS PROVIDED &quot;AS IS&quot;, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
                 </textarea>
                 <br /><br />
                 <Text variant='medium'>
