4e644c1082a4d89ba17b9b68f30a7ca4c7a8a23a
Makoto Emura <makoto@emurasoft.com>
2019-03-23 00:02:37 -0700

    .

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,5 @@
 import {Store} from '../Store';
 import * as React from 'react';
-import Export from './Export';
-import About from './About';
 import {ErrorBoundary} from './ErrorBoundary';
 import {MemoryRouter, Route} from 'react-router';
 import * as queryString from 'query-string';
@@ -16,13 +14,21 @@ function initialEntry(name): string {
     }
 }
 
-const Import = React.lazy(() => import(/* webpackChunkName: 'import' */'./Import'));
+const Import = React.lazy(
+    () => import(/* webpackChunkName: 'import', webpackPrefetch: true */'./Import')
+);
+const Export = React.lazy(
+    () => import(/* webpackChunkName: 'export', webpackPrefetch: true */'./Export')
+);
+const About = React.lazy(
+    () => import(/* webpackChunkName: 'about', webpackPrefetch: true */'./About')
+);
 
 export function App(): JSX.Element {
     return (
         <ErrorBoundary>
             <Store>
-                <React.Suspense fallback={'loading'}>
+                <React.Suspense fallback={''}>
                     <MemoryRouter
                         initialEntries={[initialEntry(queryString.parse(location.search).page)]}
                     >
