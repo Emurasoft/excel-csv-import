99a4e4a6a3a225a6b24eb0a13e3e478ea23b52dd
Makoto Emura <makoto@emurasoft.com>
2019-03-25 17:15:58 -0700

    .

--- src/Parser.ts
+++ src/Parser.ts
@@ -106,6 +106,7 @@ export class ChunkProcessor {
         this._excelAPI.setChunk(this._worksheet, this._currRow, chunk.data);
         this._currRow += chunk.data.length;
         parser.pause();
+        // sync() must be called after each chunk, otherwise API may crash
         this._worksheet.context.sync().then(parser.resume);
         // Since the Excel API is so damn slow, updating GUI every chunk has a negligible impact
         // on performance.
