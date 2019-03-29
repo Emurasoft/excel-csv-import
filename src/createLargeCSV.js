1fbfa69aac1de7b69c61fe8d06ec1ee96a111229
Makoto Emura <makoto@emurasoft.com>
2019-03-28 19:33:25 -0700

    .

---
+++ src/createLargeCSV.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+const rows = 1000000;
+const columns = 100;
+const bytesPerCell = 10;
+
+/**
+ * @returns {string}
+ */
+function row() {
+    let result = '';
+    for (let i = 0; i < columns; ++i) {
+        result += 'a'.repeat(bytesPerCell) + ',';
+    }
+    return result.substring(0, result.length - 1) + '\n';
+}
+
+function main() {
+    const rowStr = row();
+    fs.writeFileSync(__dirname + '/../largeCSVFile.csv', '');
+    const fd = fs.openSync(__dirname + '/../largeCSVFile.csv', 'a');
+    for (let i = 0; i < rows; i++) {
+        fs.writeSync(fd, rowStr);
+    }
+}
+
+main();
