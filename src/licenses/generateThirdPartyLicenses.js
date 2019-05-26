1c017ff3115bfaef316cbff9832cf4cb159e1dc5
Makoto Emura <36318069+MakotoE@users.noreply.github.com>
2019-05-25 18:03:31 -0700

    Upgrade libraries and update third party licenses (#25)

---
+++ src/licenses/generateThirdPartyLicenses.js
@@ -0,0 +1,14 @@
+const childProcess = require('child_process');
+const fs = require('fs');
+
+function main() {
+    const cwd = __dirname + '/../../'
+    const output = childProcess.execSync('yarn licenses generate-disclaimer', {cwd}).toString();
+    const trimmed = output.substring(output.indexOf('-----\n\n') + '-----\n\n'.length);
+    const escaped = trimmed.replace(/'/g, '\\\'');
+    const newlinesReplaced = escaped.replace(/\n/g, '\\n');
+    const text = 'export default \'' + newlinesReplaced + '\';';
+    fs.writeFileSync(__dirname + '/thirdParty.ts', text);
+}
+
+main();
