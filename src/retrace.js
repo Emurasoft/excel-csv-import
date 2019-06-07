fc1e912c8bdbe7eb2ce4412953be1f707264a0b4
Makoto <makoto@emurasoft.com>
2019-06-06 23:16:31 -0700

    .

--- src/retrace.js
+++ src/retrace.js
@@ -2,24 +2,6 @@ const retrace = require('retrace');
 const https = require('https');
 
 /**
- * @param {string} url
- */
-function register(url) {
-    return new Promise(resolve => {
-        https.get(url.replace('.js', '.js.map'), (res) => {
-            const chunks = [];
-            res.on('data', (data) => {
-                chunks.push(data);
-            });
-            res.on('end', () => {
-                retrace.register(url, Buffer.concat(chunks).toString());
-                resolve();
-            });
-        });
-    })
-}
-
-/**
  * @returns {Promise<string>}
  */
 function inputRawTrace() {
@@ -44,15 +26,6 @@ function inputRawTrace() {
  */
 async function main() {
     const rawTrace = await inputRawTrace();
-
-    const jsFiles = new Set(rawTrace.match(/https:\/\/emurasoft\.github\.io\/excel-csv-import\/.*\.js/g));
-
-    const promises = [];
-    for (const url of jsFiles) {
-        promises.push(register(url));
-    }
-    await Promise.all(promises);
-
     const stack = await retrace.map(rawTrace);
 
     process.stdout.write(stack + '\n');
