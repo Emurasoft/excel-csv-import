25caccc73fe7f459af1c7915952e33577b7d9392
Makoto <makoto@emurasoft.com>
2020-04-20 19:28:28 -0700

    .

---
+++ src/sourceMap.js
@@ -0,0 +1,10 @@
+const sourceMap = require('source-map');
+const fetch = require('node-fetch')
+
+async function main() {
+    const map = await fetch('https://emurasoft.github.io/excel-csv-import/export~import.8f32e9df82fd02d124e8.js.map');
+    const smc = await new sourceMap.SourceMapConsumer(await map.text());
+    console.log(smc.originalPositionFor({line: 1, column: 3307}));
+}
+
+main();
