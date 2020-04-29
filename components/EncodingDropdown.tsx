506c62967f9386bf56d9629b43ee27b36f850b52
Makoto <makoto@emurasoft.com>
2020-04-28 19:17:58 -0700

    .

--- components/EncodingDropdown.tsx
+++ components/EncodingDropdown.tsx
@@ -3,44 +3,39 @@ import * as React from 'react';
 import {Dropdown, IDropdownOption} from '@fluentui/react';
 import {EncodingDropdownOptions} from './EncodingDropdownOptions';
 
-interface Props extends BaseProps<string> {
+interface Props {
 	showAutoDetect: boolean;
 	hidden: boolean;
+	value: string;
+	onChange: (value: string) => void;
 }
 
-export class EncodingDropdown extends React.Component<Props, {}> {
-	public constructor(props: Props) {
-		super(props);
+const AutoDetectOption: IDropdownOption = {
+	'key': '',
+	'text': 'Auto-detect',
+};
 
-		const AutoDetectOption: IDropdownOption = {
-			'key': '',
-			'text': 'Auto-detect',
-		};
-
-		if (props.showAutoDetect) {
-			this._dropdownOptions = [AutoDetectOption, ...EncodingDropdownOptions];
-		} else {
-			this._dropdownOptions = EncodingDropdownOptions;
-		}
+export function EncodingDropdown({showAutoDetect, hidden, value, onChange}: Props): React.ReactElement {
+	if (hidden) {
+		return null;
 	}
 
-	public render(): React.ReactNode {
-		if (this.props.hidden) {
-			return null;
-		} else {
-			return (
-				<>
-					<Dropdown
-						label={'Encoding'}
-						selectedKey={this.props.value}
-						options={this._dropdownOptions}
-						onChange={(_, option) => this.props.onChange(option.key as string)}
-					/>
-					<br/>
-				</>
-			);
-		}
+	let dropdownOptions: IDropdownOption[];
+	if (showAutoDetect) {
+		dropdownOptions = [AutoDetectOption, ...EncodingDropdownOptions];
+	} else {
+		dropdownOptions = EncodingDropdownOptions;
 	}
 
-	private readonly _dropdownOptions: IDropdownOption[];
+	return (
+		<>
+			<Dropdown
+				label={'Encoding'}
+				selectedKey={value}
+				options={dropdownOptions}
+				onChange={(_, option) => onChange(option.key as string)}
+			/>
+			<br/>
+		</>
+	);
 }
