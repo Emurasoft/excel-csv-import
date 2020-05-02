3da2b55f83fbe42241d6161bb431452bba8e72bc
Makoto <makoto@emurasoft.com>
2020-05-02 11:42:29 -0700

    .

--- components/TitleBar.tsx
+++ components/TitleBar.tsx
@@ -2,6 +2,7 @@ import {StoredComponent} from './StoredComponent';
 import {IconButton, PrimaryButton, Text} from '@fluentui/react';
 import * as React from 'react';
 import * as style from './style.css';
+import {namespacedUseLocalStorage} from '../useLocalStorage';
 
 interface Props {
 	text: string;
@@ -9,10 +10,7 @@ interface Props {
 	mac: boolean;
 }
 
-interface State {
-	firstVisit: boolean;
-	visible: boolean;
-}
+const useLocalStorage = namespacedUseLocalStorage('app');
 
 // TitleBar contains the page title and a question mark icon in the top right corner for linking to
 // the help page. This app was designed to be self-explanatory, but Office Store policies demands
@@ -21,73 +19,57 @@ interface State {
 // proposition.
 // Validation report: Please provide additional information on the first screen explaining how to
 // use the add-in, or directing the user to help / configuration information.
-export class TitleBar extends StoredComponent<Props, State> {
-	public constructor(props: Props) {
-		super(props, 'app', {firstVisit: true, visible: false}, ['firstVisit']);
-		this._save = true;
-		let firstVisit = {};
-		try {
-			firstVisit = StoredComponent.loadState('app', ['firstVisit']);
-		} catch { // localStorage may fail
-			firstVisit = {firstVisit: false};
-		}
-		this.state = {...this.state, ...firstVisit};
-	}
+export function TitleBar({text, helpLink, mac}: Props): React.ReactElement {
+	const [firstVisit, setFirstVisit] = useLocalStorage('firstVisit', true);
 
-	public render(): React.ReactNode {
-		return (
-			<>
-				<div
-					style={{
-						width: '100%',
-						display: 'flex',
-						justifyContent: 'space-between',
-					}}
-				>
-					<Text variant='xLarge'><strong>{this.props.text}</strong></Text>
-					<div className={style.smallIcon}>
-						<IconButton
-							// Mac platform puts a big button in the top right corner
-							style={{marginRight: this.props.mac ? '30px' : '4px'}}
-							iconProps={{iconName: 'Help'}}
-							title={'Help page'}
-							ariaLabel={'Help page'}
-							href={this.props.helpLink}
-							target='_blank'
-							rel='noopener noreferrer'
-						/>
-					</div>
+	return (
+		<>
+			<div
+				style={{
+					width: '100%',
+					display: 'flex',
+					justifyContent: 'space-between',
+				}}
+			>
+				<Text variant='xLarge'><strong>{text}</strong></Text>
+				<div className={style.smallIcon}>
+					<IconButton
+						// Mac platform puts a big button in the top right corner
+						style={{marginRight: mac ? '30px' : '4px'}}
+						iconProps={{iconName: 'Help'}}
+						title={'Help page'}
+						ariaLabel={'Help page'}
+						href={helpLink}
+						target='_blank'
+						rel='noopener noreferrer'
+					/>
 				</div>
-				{
-					this.state.visible
-						? <div
-								style={{
-									position: 'absolute',
-									zIndex: 1,
-									width: '100%',
-									height: '90%',
-									backgroundColor: '#FFFFFF',
-								}}
-							>
-								<div className={style.pageMargin}>
-									<Text variant='mediumPlus'>
-										{/* eslint-disable-next-line max-len */}
-										{'CSV Import+Export can open and save CSV files of various formats. If you need any help, the "?" icon in the top right corner will take you to the help page.'}
-									</Text>
-									<br /><br />
-									<PrimaryButton
-										text={'Continue'}
-										onClick={() => this.setState({visible: false, firstVisit: false})}
-									/>
-								</div>
-							</div>
-						: null
-				}
-			</>
-		);
-	}
-
-	public componentDidMount(): void {
-		this.setState(state => ({visible: state.firstVisit}));
-	}
+			</div>
+			{
+				firstVisit
+					? <div
+						style={{
+							position: 'absolute',
+							zIndex: 1,
+							width: '100%',
+							height: '90%',
+							backgroundColor: '#FFFFFF',
+						}}
+					>
+						<div className={style.pageMargin}>
+							<Text variant='mediumPlus'>
+								{/* eslint-disable-next-line max-len */}
+								{'CSV Import+Export can open and save CSV files of various formats. If you need any help, the "?" icon in the top right corner will take you to the help page.'}
+							</Text>
+							<br /><br />
+							<PrimaryButton
+								text={'Continue'}
+								onClick={() => setFirstVisit(false)}
+							/>
+						</div>
+					</div>
+					: null
+			}
+		</>
+	);
 }
