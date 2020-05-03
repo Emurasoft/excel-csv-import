6b3a9e24b4bab67ddadd799eba3d235e33712250
Makoto <makoto@emurasoft.com>
2020-05-03 11:32:45 -0700

    .

--- index.tsx
+++ index.tsx
@@ -4,8 +4,12 @@ import {initializeIcons} from '@fluentui/react/lib/Icons';
 import * as queryString from 'query-string';
 import {Pages} from './Pages';
 import {ErrorBoundary} from './components/ErrorBoundary';
-import {Store} from './Store';
 import {MemoryRouter, Route} from 'react-router';
+import {ExtraArg} from './action';
+import thunk from 'redux-thunk';
+import {Provider} from 'react-redux';
+import {applyMiddleware, compose, createStore} from 'redux';
+import {reducer} from './reducer';
 
 initializeIcons();
 
@@ -22,21 +26,28 @@ const LicenseInformation = React.lazy(
 	() => import(/* webpackChunkName: 'license' */'./components/LicenseInformation'),
 );
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const extraArg: ExtraArg = {};
+const enhancer = composeEnhancers(applyMiddleware(thunk.withExtraArgument(extraArg)));
+const store = createStore(reducer, enhancer);
+
 function App(): JSX.Element {
-	// pageValue could be an array but resulting behavior is expected either way
-	const pageValue = queryString.parse(location.search).page as string;
+	// page could be an array but resulting behavior is expected either way
+	const page = queryString.parse(location.search).page as string;
 	return (
 		<ErrorBoundary>
-			<Store>
-				<React.Suspense fallback={''}>
-					<MemoryRouter initialEntries={[pageValue]}>
+			<React.Suspense fallback={''}>
+				<Provider store={store}>
+					<MemoryRouter initialEntries={[page]}>
 						<Route path={Pages.import} component={Import} />
 						<Route path={Pages.export} component={Export} />
 						<Route path={Pages.about} component={About} />
 						<Route path={Pages.licenseInformation} component={LicenseInformation} />
 					</MemoryRouter>
-				</React.Suspense>
-			</Store>
+				</Provider>
+			</React.Suspense>
 		</ErrorBoundary>
 	);
 }
