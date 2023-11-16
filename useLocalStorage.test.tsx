d20d8e43499029a422852282febbd50ee6c201dc
Makoto Emura <makoto@emurasoft.com>
2023-11-16 14:04:58 -0800

    useLocalStorage

--- useLocalStorage.test.tsx
+++ useLocalStorage.test.tsx
@@ -1,92 +1,36 @@
-import {useLocalStorage, namespacedUseLocalStorage} from './useLocalStorage';
+import {namespacedUseLocalStorage} from './useLocalStorage';
 import * as React from 'react';
-import * as assert from 'assert';
 import {describe, beforeEach, expect, test} from '@jest/globals';
+import {render} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
-// describe('useLocalStorage', () => {
-// 	beforeEach(() => window.localStorage.clear());
+describe('useLocalStorage', () => {
+	beforeEach(() => window.localStorage.clear());
 
-// 	it('set and get', () => {
-// 		function E(): React.ReactElement {
-// 			const [v, setV] = useLocalStorage('key', 0);
-// 			return (
-// 				<>
-// 					<p>{v}</p>
-// 					<button onClick={() => setV(1)} />
-// 				</>
-// 			);
-// 		}
+	test('set and get', async () => {
+		function E(): React.ReactElement {
+			const [v, setV] = namespacedUseLocalStorage('namespace')('key', 0);
+			return (
+				<>
+					<p>{v}</p>
+					<button onClick={() => setV(1)} />
+				</>
+			);
+		}
 
-// 		{
-// 			const wrapper = shallow(<E />);
-// 			assert.strictEqual(wrapper.find('p').text(), '0');
-// 			assert.strictEqual(window.localStorage.getItem('key'), null);
-// 			wrapper.find('button').simulate('click');
-// 			assert.strictEqual(wrapper.find('p').text(), '1');
-// 			assert.strictEqual(window.localStorage.getItem('key'), '1');
-// 		}
-// 		{
-// 			const wrapper = shallow(<E />);
-// 			assert.strictEqual(wrapper.find('p').text(), '1');
-// 		}
-// 	});
+		{
+			const element = render(<E />);
+            expect(localStorage.getItem('namespace-key')).toBeNull();
 
-// 	it('empty key', () => {
-// 		function E(): React.ReactElement {
-// 			const [v, setV] = useLocalStorage('', 0);
-// 			return (
-// 				<>
-// 					<p>{v}</p>
-// 					<button onClick={() => setV(1)} />
-// 				</>
-// 			);
-// 		}
+            await userEvent.click(element.getByRole('button'));
+            expect(localStorage.getItem('namespace-key')).toEqual('1');
 
-// 		{
-// 			const wrapper = shallow(<E />);
-// 			wrapper.find('button').simulate('click');
-// 			assert.strictEqual(window.localStorage.getItem(''), '1');
-// 		}
-// 		{
-// 			const wrapper = shallow(<E />);
-// 			assert.strictEqual(wrapper.find('p').text(), '1');
-// 		}
-// 	});
-
-// 	it('namespacedUseLocalStorage', () => {
-// 		const useLocalStorage0 = namespacedUseLocalStorage('namespace');
-
-// 		function E(): React.ReactElement {
-// 			const [v, setV] = useLocalStorage0('key', 0);
-// 			return (
-// 				<>
-// 					<p>{v}</p>
-// 					<button onClick={() => setV(1)} />
-// 				</>
-// 			);
-// 		}
-
-// 		{
-// 			const wrapper = shallow(<E />);
-// 			wrapper.find('button').simulate('click');
-// 			assert.strictEqual(window.localStorage.getItem('namespace-key'), '1');
-// 		}
-// 		{
-// 			const wrapper = shallow(<E/>);
-// 			assert.strictEqual(wrapper.find('p').text(), '1');
-// 		}
-// 		const useLocalStorage1 = namespacedUseLocalStorage('differentNamespace');
-// 		function DifferentElement(): React.ReactElement {
-// 			const [v] = useLocalStorage1('key', 0);
-// 			return (
-// 				<>
-// 					<p>{v}</p>
-// 				</>
-// 			);
-// 		}
-// 		{
-// 			const wrapper = shallow(<DifferentElement />);
-// 			assert.strictEqual(wrapper.find('p').text(), '0');
-// 		}
-// 	});
-// });
+            element.baseElement.innerHTML = '';
+		}
+		{
+            // Simulate reload app
+			const element = render(<E />);
+            expect(element.baseElement.textContent).toEqual('1');
+		}
+	});
+});
