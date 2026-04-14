a28e087d499cf8ffaf0f6e0c6da199f443441a3b
Makoto Emura <makoto@emurasoft.com>
2026-04-14 15:48:24 -0700

    type

--- errorhandler.ts
+++ errorhandler.ts
@@ -1,9 +1,11 @@
-import {errorOutput, SET_OUTPUT} from './action';
+import {Dispatch, errorOutput, SET_OUTPUT} from './action';
 
+// @ts-ignore
 export const errorHandler = ({dispatch}) => next => async (action) => {
 	try {
 		return await next(action);
 	} catch (error) {
+// @ts-ignore
 		dispatch({type: SET_OUTPUT, output: errorOutput(error)});
 		throw error;
 	}
