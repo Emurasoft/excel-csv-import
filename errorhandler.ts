e3e9a5c3acbbbd708d068796eed533bb8df7097b
Makoto Emura <makoto@emurasoft.com>
2026-07-16 23:11:59 -0700

    lint

--- errorhandler.ts
+++ errorhandler.ts
@@ -1,9 +1,11 @@
 import { errorOutput, SET_OUTPUT } from './action';
 
-// @ts-expect-error
 export const errorHandler =
+// @ts-expect-error
 	({ dispatch }) =>
+// @ts-expect-error
 	(next) =>
+// @ts-expect-error
 	async (action) => {
 		try {
 			return await next(action);
